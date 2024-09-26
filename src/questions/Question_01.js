f20ccba64b4712a618032936a8bbd7539bc4cd39
admin <admin@AdminIMac.local>
2024-09-26 10:59:31 +0900

    コミット

--- src/questions/Question_01.js
+++ src/questions/Question_01.js
@@ -23,8 +23,8 @@ export default function Question_01() {
 
   // 定数群
   const message = "Hello World!";   // 変更しない
-  const date = "";                  // 本日の日付 (YYYY/MM/DD)
-  const id = "";                    // あなたの学籍番号
+  const date = "竹井翼";                  // 本日の日付 (YYYY/MM/DD)
+  const id = "13";                    // あなたの学籍番号
 
   return (
     <div>
